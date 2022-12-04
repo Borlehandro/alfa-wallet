7319e19ff9fa91dec4e9a514eac89d148948ebe3
Borlehandro <aborzikov@decentury.io>
2022-12-04 20:04:43 +0700

    Initial: Setup backend project

---
+++ backend/AlfaWallet/src/test/java/com/alfawallet/backend/AlfaWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfawallet.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfaWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
